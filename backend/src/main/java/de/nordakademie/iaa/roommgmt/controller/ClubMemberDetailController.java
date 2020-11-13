77bfdd8d04ce942319c974ca7113278f0e86d46b
Torben Labs <torbenlabs@web.de>
2020-11-14 00:10:10 +0100

    Add Request MApping for club member form

--- backend/src/main/java/de/nordakademie/iaa/roommgmt/controller/ClubMemberDetailController.java
+++ backend/src/main/java/de/nordakademie/iaa/roommgmt/controller/ClubMemberDetailController.java
@@ -4,9 +4,13 @@ import de.nordakademie.iaa.roommgmt.model.ClubMember;
 import de.nordakademie.iaa.roommgmt.service.ClubMemberService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.inject.Inject;
+import java.util.List;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @RestController
 public class ClubMemberDetailController {
@@ -14,10 +18,18 @@ public class ClubMemberDetailController {
     private ClubMemberService clubMemberService;
 
     @GetMapping("/club-member-form/{id}")
-    ClubMember one(@PathVariable Long id){
+    ClubMember one(@PathVariable Long id) {
         System.out.println(id);
         return clubMemberService.loadClubMember(id);
 
+    }
+
+    @RequestMapping(path = "/club-member-form/{id}")
+    public class ClubMemberOverviewController {
+        @RequestMapping(method = GET)
+        public ClubMember loadClubMember(@PathVariable Long id) {
+            return clubMemberService.loadClubMember(id);
+        }
     }
 
     @Inject
