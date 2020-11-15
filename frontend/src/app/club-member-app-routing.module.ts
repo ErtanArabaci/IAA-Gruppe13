c3b7d8762700935b3f8af8b1ecf95128cd6e180d
ertan <ertan.arabaci@hotmail.de>
2020-11-15 17:22:48 +0100

    Delete routing for club-members

--- frontend/src/app/club-member-app-routing.module.ts
+++ frontend/src/app/club-member-app-routing.module.ts
@@ -2,14 +2,12 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
 import {ClubMemberFormComponent} from "./components/club-member-form/club-member-form.component";
 import {ClubMemberListComponent} from "./components/club-member-list/club-member-list.component";
-import {ClubMembersComponent} from "./components/club-members/club-members.component";
 import {ClubMemberAccountingComponent} from "./components/club-member-accounting/club-member-accounting.component";
 import {ClubMemberCreateFormComponent} from "./components/club-member-create-form/club-member-create-form.component";
 
 const routes: Routes = [
   {path: 'club-member-form/:id', component: ClubMemberFormComponent},
   {path: 'club-member-form', component: ClubMemberFormComponent},
-  {path: 'club-members', component: ClubMembersComponent},
   {path: 'club-member-list', component: ClubMemberListComponent},
   {path: 'club-member-accounting', component: ClubMemberAccountingComponent},
   {path: 'club-member-accounting/:id', component: ClubMemberAccountingComponent},
