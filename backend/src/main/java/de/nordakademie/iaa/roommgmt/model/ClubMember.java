5aae5f3a5c9aa688b693ba780e33619923ca3bf1
Torben Labs <torbenlabs@web.de>
2020-11-15 02:38:11 +0100

    overlay reworked

--- backend/src/main/java/de/nordakademie/iaa/roommgmt/model/ClubMember.java
+++ backend/src/main/java/de/nordakademie/iaa/roommgmt/model/ClubMember.java
@@ -1,5 +1,7 @@
 package de.nordakademie.iaa.roommgmt.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
